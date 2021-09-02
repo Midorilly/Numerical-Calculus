e5d701a333075466bd6b346ae41b32c776af27b7
Midorilly <angelica.ghizzota@gmail.com>
2021-09-03 01:12:19 +0200

    Refactor

--- linear_algebra.py
+++ linear_algebra.py
@@ -5,12 +5,14 @@ Created on Thu Jul 22 01:08:56 2021
 @author: angel
 """
 
-from numpy import set_printoptions, transpose, matmul, nan, zeros, copy, identity, triu, shape, delete, array, hstack, vstack, full
+from numpy import dot, set_printoptions, transpose, matmul, nan, zeros, copy, identity, triu, shape, delete, array, hstack, vstack
+from numpy.linalg import inv
 
-set_printoptions(precision=3)
+set_printoptions(precision=4)
 
 def transposition(A):
     """
+    OK
     Transpose of a m-by-n matrix in a n-by-m matrix.
 
     Parameters
@@ -32,18 +34,19 @@ def transposition(A):
 
 def leading_principal_submatrix(A, k):
     """
+    OK
     Leading principal sumbatrix of A only considers the first k rows and k columns of A
 
     Parameters
     ----------
-    A : bidimensional array
+    A : bidimensional array.
         Matrix.
-    k : int
+    k : int.
         Number of rows and columns of A which remain.
 
     Returns
     -------
-    A : bidimensional array
+    A : bidimensional array.
         Leading principal sumbatrix.
     """
     [m,n] = shape(A)
@@ -60,8 +63,9 @@ def leading_principal_submatrix(A, k):
             A = delete(A, j, axis=1)
         return A
 
-def laplace(A):
+def laplace(M):
     """
+    OK
     Laplace expansion for computing the determinant of a square matrix.
     Progression along the first row.
 
@@ -75,6 +79,7 @@ def laplace(A):
     det : float
         Determinant of A.
     """
+    A = copy(M)
     [m,n] = shape(A)
     if m == n:
         if n == 1:
@@ -89,8 +94,39 @@ def laplace(A):
     else:
         print('Warning: The input matrix is not square\nUnable to compute its determinant')
         
+def rank(A):
+    """
+    Rank of a m-by-n matrix, computed counting non-null rows of its echelon form.
+    The rank of a matrix can be defined in different equivalent ways:
+        -  largest order of any non-zero minor in A
+        -  dimension of the vector space generated (or spanned) by its columns
+        -  maximal number of linearly independent columns of A
+        -  dimension of the vector space spanned by its rows
+        -  measure of the "nondegenerateness" of the system of linear equations 
+           and linear transformation encoded by A
+
+    Parameters
+    ----------
+    A : bidimensional array
+        Matrix.
+
+    Returns
+    -------
+    rank : int
+        DESCRIPTION.
+
+    """
+    M = echelon_form(A)        
+    [m,n] = shape(M)
+    rank = 0
+    for i in range(m):
+        if any((M[i,:] != 0)): #counts non-null rows
+            rank = rank + 1            
+    return rank        
+        
 def upper_triangular(A, b):
     """
+    OK
     Algorithm for back substitution for resolving upper triangular linear system
 
     Parameters
@@ -98,7 +134,7 @@ def upper_triangular(A, b):
     A : bidimensional array
         Upper triangular matrix of coefficients. Must be a square matrix
     b : array
-        Columns vector m-by-1 of known terms. 
+        Columns vector n-by-1 of known terms. 
 
     Returns
     -------
@@ -107,7 +143,7 @@ def upper_triangular(A, b):
     """
     [m,n] = shape(A)
     if m == n:
-        x = zeros((n,1))
+        x = zeros((n))
         if laplace(A) == 0: #det(A) != 0 iff A[i,i] != 0 for i in range(0, n-1)
             print('Warning: the matrix is singular\nUnable to solve the system of linear equation')
             return nan
@@ -115,14 +151,16 @@ def upper_triangular(A, b):
             for i in range(m-1, -1, -1):
                 sum = 0
                 for j in range(i+1, n):
-                    sum = sum + A[i,j]*x[j,0]   
-                x[i,0] = (b[0,i]-sum)/A[i,i]
+                    sum = sum + A[i,j]*x[j]   
+                x[i] = (b[0,i]-sum)/A[i,i]
+                
             return x
     else:
         print('Error: the matrix is not square\nUnable to solve the system of linear equation')
             
 def lower_triangular(A, b):
     """
+    OK
     Algorithm for forward substitution for resolving lower triangular linear system
 
     Parameters
@@ -141,7 +179,7 @@ def lower_triangular(A, b):
     if m == n:
         x = zeros((n,1))
         if laplace(A) == 0: #det(A) != 0 iff A[i,i] != 0 for i in range(0, n-1)
-            print('Warning: the matrix is singular\nUnable to solve the system of linear equation')
+            print('Warning: the matrix is singular\nUnable to solve the system of linear equations')
             return nan
         else:
             for i in range(0, m):
@@ -151,10 +189,11 @@ def lower_triangular(A, b):
                 x[i,0] = (b[i]-sum)/A[i,i]
             return x
     else:
-        print('Error: the matrix is not square\nUnable to solve the system of linear equation')
-    
+        print('Error: the matrix is not square\nUnable to solve the system of linear equations')
+
 def lu_fact(A):
     """
+    OK
     LU factorization of a square matrix.
 
     Parameters
@@ -188,39 +227,23 @@ def lu_fact(A):
         return L, U          
     else:
          print('Error: the matrix is not square\nUnable to decompose the matrix')
-         return
-       
-def rank(A):
+         return    
+     
+def echelon_form(M):
     """
-    Rank of a m-by-n matrix, computed counting non-null rows of its echelon form.
-    The rank of a matrix can be defined in different equivalent ways:
-        -  largest order of any non-zero minor in A
-        -  dimension of the vector space generated (or spanned) by its columns
-        -  maximal number of linearly independent columns of A
-        -  dimension of the vector space spanned by its rows
-        -  measure of the "nondegenerateness" of the system of linear equations 
-           and linear transformation encoded by A
+    ???? DA RIVEDERE
 
     Parameters
     ----------
-    A : bidimensional array
-        Matrix.
+    M : TYPE
+        DESCRIPTION.
 
     Returns
     -------
-    rank : int
+    TYPE
         DESCRIPTION.
 
     """
-    M = echelon_form(A)        
-    [m,n] = shape(M)
-    rank = 0
-    for i in range(m):
-        if any((M[i,:] != 0)): #counts non-null rows
-            rank = rank + 1            
-    return rank
-    
-def echelon_form(M):
     # Base case
     # if r == 0 or c == 0 -> M is already in its echelon form
     # if M.r == 1 or M.c == 1 -> one remaining element
@@ -253,48 +276,138 @@ def echelon_form(M):
     MR = echelon_form(M[1:,1:])
 
     # Riaggiungiamo la riga e la colonna rimossa al risultato dell'operazione
-    return vstack([M[:1], hstack([M[1:,:1], MR])])    
+    return vstack([M[:1], hstack([M[1:,:1], MR])])        
     
-def linear_independence(*vectors):
+def inverse_linear_system(A, b):
     """
-    Determine which vectors are linear independent.
-    The algorithm inserts every column-like vector in an emtpy matrix, then proceeds to reduce this
-    matrix to echelon form; indexes of pivot columns are searched and stored in a list by pivot_column()
+    OK
+    Solves a linear system when A is a square, non-singular matrix computing x = A^-1*b
 
     Parameters
     ----------
-    *vectors : array
-        Arbitraty number of vectors. 
+    A : bidimensional array.
+        Square, non-singular matrix.
+    b : array
+        Columns vector n-by-1 of known terms. 
 
     Returns
     -------
-    independent : array
-        Array of linear independent vectors.
-    pivot : array
-        Array of linear independent vectors column indexes.
-    """  
-    c = len(vectors)
-    r = len(vectors[0])   
-    
-    M = full((c, r), vectors)
-    M = transposition(M)
-    print('Vectors: \n', M)
-    
-    E = copy(M)  
-    E = echelon_form(E)
-    pivot = pivot_column(E)
-       
-    independent = []
-    for p in pivot:
-        independent.append(M[:,p])
-        
-    independent = array(independent)
-    independent = transposition(independent)
-    print('Column index(es) of linear independet vectors: ')
-    return pivot, independent
-    
-def pivot_column(M):
+    x : array
+        Column vector n-by-1 of solutions of the linear system.
+
     """
+    if laplace(A) != 0:
+        Ai = inv(A)
+        x = dot(Ai,b)
+        return x
+    else: 
+        print('Warning: the matrix is singular\nUnable to solve the system of linear equations')
+        return nan
+    
+def lu_linear_system(A, b):
+    """
+    OK
+    Solves a linear system when A is a square matrix computing its LU factorization. 
+
+    Parameters
+    ----------
+    A : bidimensional array.
+        Square matrix.
+    b : array
+        Columns vector n-by-1 of known terms. 
+
+    Returns
+    -------
+    x : array
+        Column vector n-by-1 of solutions of the linear system.
+
+    """
+    M = copy(A)
+    L, U = lu_fact(M)
+    print(L, U)
+    
+    y = lower_triangular(L, b)
+    x = upper_triangular(U, y)
+       
+    return x
+    
+def gauss_elimination(A, b):
+    """
+    OK
+    Implements Gauss elimination algorithm to solve m-equations, n-variable systems.
+
+    Parameters
+    ----------
+    A : bidimensional array
+        Matrix of coefficients, m-by-n. 
+    b : array
+        Columns vector m-by-1 of known terms. 
+
+    Returns
+    -------
+    x : array
+        Vector n-by-1 of variables.
+
+    """
+    B = copy(A)
+    d = copy(b)
+    
+    consistency, solution = is_consistent(A, b)
+    if(consistency):
+        parameter = []
+        for i in range(0, solution):
+            param = float(input('Please type the chosen real value for parameter'))
+            parameter.append(param)
+            i = i+1
+           
+        d = transpose(d)
+        
+        #creates complete matrix
+        complete = hstack((B, d))
+        echelon = echelon_form(complete)
+        
+        #verifies if the echelon form contains any null row and deletes it
+        [m,n] = shape(echelon)
+        i = 0
+        for i in range(m):
+            if all((echelon[i,:] == 0)): 
+                echelon = delete(echelon, i, axis=0)
+                m = m-1
+      
+        
+        #separates the matrix of coefficients and the array of known terms to perform the 
+        #back sostituition algorithm and obtain an array of variables
+        [m,n] = shape(echelon)
+        known = array([echelon[:, n-1]], dtype=float)
+        coefficient = delete(echelon, n-1, axis=1)
+        
+        #determines if the coefficient matrix is square; if not, parametrization is applied
+        [k,l] = shape(coefficient)
+        if k != l: #parametrization          
+            p_row, p_col = pivot_index(coefficient)
+            i = 0
+            j = 0
+            p = 0
+            for p in range(solution):
+                for j in range(l):
+                    if j in p_col:  
+                        j = j+1
+                    else:
+                        for i in range(k):                       
+                            known[0, i] = known[0, i] - coefficient[i,j]*parameter[p]  
+                        coefficient = delete(coefficient, j, axis=1)   
+                        j = j+1
+                        
+            x = upper_triangular(coefficient, known)
+            
+        else:    
+            x = upper_triangular(coefficient, known)
+        
+        return x
+    
+def pivot_index(M):
+    """
+    DA RIVEDERE
     Parameters
     ----------
     M : bidimensional array
@@ -302,67 +415,114 @@ def pivot_column(M):
 
     Returns
     -------
-    pivot : array
+    pivot: array
         Array of pivot columns indexes.
     """
     [m,n] = shape(M)
-    pivot = []
+    pivot_row = []
+    pivot_column = []
     i = 0
     j = 0
     for i in range(m):
         for j in range(n):
             if M[i,j] != 0:
-                pivot.append(j)
+                pivot_row.append(i)
+                pivot_column.append(j)
                 break
-    return pivot
+    return pivot_row, pivot_column
 
-def linear_least_squares(A, b):
-    At = copy(A)
-    A = transpose(A)
-    b = transpose(b)
-    
-    R = copy(A)
-    [m,n] = shape(R)
-    if rank(R) != min(m,n):
-        print('Error: the rank of the input matrix is not its maximum \nUnable to solve the problem')
-        return
-    
-    #calcolo AtA e Atb
-    AtA = matmul(At,A)
-    #print('At*A =\n', AtA) 
-    Atb = matmul(At,b)
-    #print('At*b =\n', Atb)
-    
-    #eliminazione di Gauss
-    x = gauss_elimination(AtA, Atb)
-    return x
-    #print(complete)
-    
-def gauss_elimination(A, b):
-    complete = hstack((A, b))
-    echelon = echelon_form(complete)
-    
-    [i,j] = shape(echelon)
-    known = array([echelon[:, j-1]], dtype=float)
-    coefficient = delete(echelon, j-1, axis=1)
-    variable = upper_triangular(coefficient, known)
-    return variable
+def linear_least_squares(M, v):
+    """
+    OK
+    Solves the linear least squares problem.
+    If rank(A) is its maximum then the linear least squares problem has one unique solution, obtained
+    as solution of a n-equations, n-variables system AtAx = Atb, known as normal system
+    Parameters
+    ----------
+    A : bidimensional array
+        Matrix of coefficients. 
+    b : array
+        Columns vector m-by-1 of known terms. 
 
-def generating_set(*vectors):
-    
-    c = len(vectors)
-    r = len(vectors[0])   
-    
-    incomplete = full((c, r), vectors)
-    incomplete = transposition(incomplete)
-    M = copy(incomplete)
-    rank_i = rank(M)
-    print(rank_i)
+    Returns
+    -------
+    x : array
+        Column vector n-by-1 of solutions of the linear system.
+
+    """
+   
+    B = copy(M)
+    [m,n] = shape(B)
+    if rank(B) != min(m,n):
+        print('Warning: can not be solved since the rank of the matrix is not its maximum value')
+        return nan
+    else:
         
-    return
+        A = copy(M)
+        At = transpose(M)
+        b = copy(v)
+        b = transpose(b)
+        
+        AtA = dot(At, A)
+        Atb = transpose(dot(At, b))
+        print(AtA, Atb)
+        
+        x = gauss_elimination(AtA, Atb)
+        print('x*:')
+        return x
+
+def is_consistent(M, v):
+    """
+    OK
+    Determines whether a linear system is consistent (has at least a solution) or not. A linear system
+    is consistent when the rank of matrix of coefficients and the rank of the complete matrix has
+    the same value (Rouché-Capelli theorem)
+
+    Parameters
+    ----------
+    A : bidimensional array.
+        Matrix of coefficients, m-by-n
+    b : array.
+        Columns vector m-by-1 of known terms.
+
+    Returns
+    -------
+    is_consistent : boolean.
+
+    """
+    A = copy(M)
+    b = copy(v)
+    [m,n] = shape(A)
+    b = transpose(b)
+    is_consistent = False
+    #computes incomplete matrix rank
+    incomplete = copy(A)
+    rank_i = rank(incomplete)
+    #computes complete matrix rank
+    complete = hstack((A, b))
+    rank_c = rank(complete)
     
+    #compares ranks
+    solution = 0
+    if abs(rank_i == rank_c):
+        is_consistent = True
+        print('The linear system is consistent.')
+        solution = n - rank_i
+        if solution == 0:
+            print('It does admit only one solution.')
+        else:
+            print('It does admit Inf^', solution, 'solution(s):')
+            print('they are infinite and depend on', solution, 'parameter(s).')
+    else:
+        print('The linear system in inconsistent.')
+        print('It does not admit any solution.')
+        print('Incomplete matrix has rank: ', rank_i, '\nComplete matrix has rank: ', rank_c)  
+          
+    return is_consistent, solution
+        
     
-    
+
+           
 
 #TEST UPPER TRIANGULAR LINEAR SYSTEM
 # A = array([[1, -2, 3], [0, -10, 13], [0, 0, 1]])
@@ -381,18 +541,19 @@ def generating_set(*vectors):
 # D = array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 # E = array([[-1,2,-1,2,3], [0,1,0,1,1], [-2,2,-2,1,3]], dtype=float)
 
-#TEST LINEAR INDEPENDENCE
-#v1 = [-1,2,-1,2,3]
-#v2 = [0,1,0,1,1]
-#v3 = [-2,2,-2,1,3]
-#v4 = [1,1,1,1,1]
+#INCONSISTENT SYSTEM
+#A = array([[1,1,-2], [1,-1,1], [2,0,-1]])
+#b = array([[0,1,0]])
 
-#v5 = [-2,0,1,2]
-#v6 = [-1,-1,2,-2]
-#v7 = [2,2,-2,-2]
+#CONSISTENT SYSTEM
+#A = array([[1,1,-2], [1,-1,1], [2,0,-1]])
+#b = array([[0,1,1]])
 
-#v1 = [-1, 1, -1]
-#v2 = [-3, 3, -3]
-#v3 = [2, 1, 5]
-#v4 = [-3, 0, -6]
-#pivot = [0,2]
+#SQUARE_LINEAR_SYSTEM
+#B = array([[1, 1, 0], [2, 1, 1], [3, 0, 1]], dtype=float)
+#b = array([[1,1,1]], dtype=float)
+#X = [0.5, 0.5, -0.5]
+
+#A = array([[-1, 0, -2], [2,1,2], [-1,0,-2], [2,1,1], [3,1,3]], dtype=float)
+#b = array([[1,1,1,1,1]], dtype=float)
+#x* = [0, 9/5, -2/5]
